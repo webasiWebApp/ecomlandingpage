70455ee966be086ba2cdcdfb8e49d7e221a8ee72
WEBASI <118072521+webasiWebApp@users.noreply.github.com>
2026-08-13 10:42:11 +0530

    Update promotional elements and pricing details in index.html

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.static(__dirname));
 
 // Serve index.html
